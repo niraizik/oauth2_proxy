ee44038e40f426be5bdb7e0a9fa7c4901379d57b
Nir Aizik <nir@veego.io>
2020-08-25 18:24:20 +0300

    fix

--- oauthproxy.go
+++ oauthproxy.go
@@ -844,9 +844,13 @@ func (p *OAuthProxy) OAuthCallback(rw http.ResponseWriter, req *http.Request) {
 // AuthenticateOnly checks whether the user is currently logged in
 func (p *OAuthProxy) AuthenticateOnly(rw http.ResponseWriter, req *http.Request) {
 	isPreflightRequestAllowed := p.skipAuthPreflight && req.Header.Get("X-Original-Method") == "OPTIONS"
+	if isPreflightRequestAllowed {
+		rw.WriteHeader(http.StatusAccepted)
+		return
+	}
 
 	session, err := p.getAuthenticatedSession(rw, req)
-	if err != nil && isPreflightRequestAllowed == false {
+	if err != nil {
 		http.Error(rw, "unauthorized request", http.StatusUnauthorized)
 		return
 	}
